9ba928e0afb8f777530ec912527ed94db5cb1989
Masoud <masoudtabatabaei20@gmail.com>
2022-04-14 10:59:22 +0430

    Implement get card data and randomize data to be placed randomly

---
+++ script.js
@@ -0,0 +1,22 @@
+const lives = document.querySelector(".lives");
+const playground = document.querySelector(".playground");
+const livesCount = 6;
+
+lives.textContent = livesCount;
+
+// card data
+const getData = () => [
+    {imageSrc: "./images/coding.png" , name: "coding"},
+    {imageSrc: "./images/docker.png" , name: "docker"},
+    {imageSrc: "./images/github.png" , name: "github"},
+    {imageSrc: "./images/internet.png" , name: "internet"},
+    {imageSrc: "./images/js.png" , name: "javascript"},
+    {imageSrc: "./images/json-file.png" , name: "json-file"},
+    {imageSrc: "./images/structure.png" , name: "structure"},
+    {imageSrc: "./images/typescript.png" , name: "typescript"}
+];
+
+// randomize data
+const randomizeData = () => {
+    return getData().sort(() => Math.random() - 0.5);
+}
