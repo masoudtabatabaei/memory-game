4bfa67795b8db98ff0203a30ae2d5c1ea17ab0e2
Masoud <masoudtabatabaei20@gmail.com>
2022-04-16 23:42:29 +0430

    Put cards on playground

--- script.js
+++ script.js
@@ -6,25 +6,42 @@ lives.textContent = livesCount;
 
 // card data
 const getData = () => [
-    {imageSrc: "./images/coding.png" , name: "coding"},
-    {imageSrc: "./images/docker.png" , name: "docker"},
-    {imageSrc: "./images/github.png" , name: "github"},
-    {imageSrc: "./images/internet.png" , name: "internet"},
-    {imageSrc: "./images/js.png" , name: "javascript"},
-    {imageSrc: "./images/json-file.png" , name: "json-file"},
-    {imageSrc: "./images/structure.png" , name: "structure"},
-    {imageSrc: "./images/typescript.png" , name: "typescript"},
-    {imageSrc: "./images/coding.png" , name: "coding"},
-    {imageSrc: "./images/github.png" , name: "github"},
-    {imageSrc: "./images/typescript.png" , name: "typescript"},
-    {imageSrc: "./images/docker.png" , name: "docker"},
-    {imageSrc: "./images/js.png" , name: "javascript"},
-    {imageSrc: "./images/structure.png" , name: "structure"},
-    {imageSrc: "./images/json-file.png" , name: "json-file"},
-    {imageSrc: "./images/internet.png" , name: "internet"},
+  {imageSrc: "./images/coding.png", name: "coding"},
+  {imageSrc: "./images/docker.png", name: "docker"},
+  {imageSrc: "./images/github.png", name: "github"},
+  {imageSrc: "./images/internet.png", name: "internet"},
+  {imageSrc: "./images/js.png", name: "javascript"},
+  {imageSrc: "./images/json-file.png", name: "json-file"},
+  {imageSrc: "./images/structure.png", name: "structure"},
+  {imageSrc: "./images/typescript.png", name: "typescript"},
+  {imageSrc: "./images/coding.png", name: "coding"},
+  {imageSrc: "./images/github.png", name: "github"},
+  {imageSrc: "./images/typescript.png", name: "typescript"},
+  {imageSrc: "./images/docker.png", name: "docker"},
+  {imageSrc: "./images/js.png", name: "javascript"},
+  {imageSrc: "./images/structure.png", name: "structure"},
+  {imageSrc: "./images/json-file.png", name: "json-file"},
+  {imageSrc: "./images/internet.png", name: "internet"},
 ];
 
 // randomize data
 const randomizeData = () => {
-    return getData().sort(() => Math.random() - 0.5);
+  return getData().sort(() => Math.random() - 0.5);
 }
+
+const createCards = () => {
+  let playgroundCards = '';
+  randomizeData().forEach(card => {
+    playgroundCards += `
+        <div class="card-container">
+            <div class="card">
+                <img src="${card.imageSrc}">
+            </div>
+        </div>
+    `;
+  });
+
+  playground.innerHTML = playgroundCards;
+}
+
+createCards();
